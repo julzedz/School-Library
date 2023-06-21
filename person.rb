39807288987cbe664dd6636029e0c48dfa69093b
user <user@users-MacBook-Air.local>
2023-06-21 04:07:28 +0100

    Correct lint errors

--- person.rb
+++ person.rb
@@ -1,22 +1,21 @@
 class Person
-  attr_reader :id,
-  attr_accessor :name
-  attr_accessor :age
-
-  def initialize(id, name = "Unknown", age, parent_permission = true)
-    @id  = Random.rand(1...1000)
+  def initialize(_id, age, name = 'Unknown', parent_permission: true)
+    @id = Random.rand(1...1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
   end
 
-  def of_age?
-    return true if @age >= 18
-    false
-  end
+  attr_reader :id
+  attr_accessor :name, :age
 
   def can_use_services?
-    return true if of_age? || parent_permission
-    false
+    of_age? || parent_permission
+  end
+
+  private
+
+  def of_age?
+    @age >= 18
   end
 end
