e042ebf62be6910e314667aa8881275f5a7b4b90
user <user@users-MacBook-Air.local>
2023-06-22 01:30:17 +0100

    Implement correct_name method in Person Class

--- person.rb
+++ person.rb
@@ -1,15 +1,18 @@
 require './nameable'
 
-class Person
+class Person < Nameable
   def initialize(_id, age, name = 'Unknown', parent_permission: true)
     @id = Random.rand(1...1000)
-    @name = name
     @age = age
     @parent_permission = parent_permission
   end
 
   attr_reader :id
   attr_accessor :name, :age
+
+  def correct_name
+    @name
+  end
 
   def can_use_services?
     of_age? || parent_permission
